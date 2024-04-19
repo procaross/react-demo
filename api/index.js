39dbb98bd1d41899a01d82f7d3ab10bcab7d3b6d
Proca <lijianhong@co.link>
2024-04-19 12:31:45 +0800

    chore(backend): 删除无关代码

--- api/index.js
+++ api/index.js
@@ -4,13 +4,6 @@ import express from "express";
 import pkg from "@prisma/client";
 import morgan from "morgan";
 import cors from "cors";
-import { auth } from "express-oauth2-jwt-bearer";
-
-const requireAuth = auth({
-  audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_ISSUER,
-  tokenSigningAlg: "RS256",
-});
 
 const app = express();
 
